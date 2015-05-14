352c9cc475e8385d3f9125965be80c9675faefe1
frusciante88 <alessandro.relati@gmail.com>
2015-05-14 21:50:36 +0200

    load file

---
+++ app.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Risorse statiche
+app.use('/www', express.static(__dirname + '/www'));
+
+app.get('/', function(req, res){
+    var options = {
+    root: __dirname + '/www/',
+    dotfiles: 'deny',
+    headers: {
+        'x-timestamp': Date.now(),
+        'x-sent': true
+    }
+  };
+
+  var fileName = 'home.html';
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+      console.log(err);
+      res.status(err.status).end();
+    }
+    else {
+      console.log('Sent:', fileName);
+    }
+  });
+});
+
+var server = app.listen(3000, function () {
+
+    var host = server.address().address;
+    var port = server.address().port;
+    
+    console.log('Example app listening at http://%s:%s', host, port)
+
+});
+
